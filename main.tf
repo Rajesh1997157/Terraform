cf36396c113de5a20273692beeda1422ac3ec917
Rajesh <rajeshshivarathri98@gmail.com>
2025-06-10 18:31:38 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,14 +1,12 @@
 provider "aws" {
-  region     = "us-east-1"
-  access_key = "AKIA2MNVL3KL6VVVMO5Q"
-  secret_key = "P/3+4tedlZhaWc7OLjXeUHNS+/QodFpThGLV7nHt"
+  region = "us-east-1"
 }
+
 resource "aws_instance" "example" {
-  count         = 3
   ami           = "ami-0c02fb55956c7d316" # Amazon Linux 2 AMI (us-east-1)
   instance_type = "t2.micro"
 
   tags = {
-    Name = "ExampleInstance-${count.index + 1}"
+    Name = "ExampleInstance"
   }
 }
